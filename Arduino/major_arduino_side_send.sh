e5ff7f97c34d0f6560c74181158684c0fa8b8da9
Ankit Srivastava <ankitks1512@gmail.com>
2016-07-01 20:19:55 +0530

    Update major_arduino_side_send.sh

--- Arduino/major_arduino_side_send.sh
+++ Arduino/major_arduino_side_send.sh
@@ -3,7 +3,7 @@ sed 's/.$//' arduino_sensor_data.txt
 a=`tail -1 arduino_sensor_data.txt`
 #a=("${@}")
 $a
-curl --data "command_string=$a&position=1" https://api.thingspeak.com/talkbacks/4578/commands.json?api_key=R7333DX3WOMRQMF7
+curl --data "command_string=$a&position=1" https://api.thingspeak.com/talkbacks/"your channel id"/commands.json?api_key="your api key"
 
 #updating the channel as well
 curl --data "field1=$a" https://api.thingspeak.com/update?api_key=N185TJWSP1F4LC2J 
