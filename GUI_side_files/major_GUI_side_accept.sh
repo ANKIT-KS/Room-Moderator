c7be075da4f6640674a1f2e7f1c291f13cd51ac2
Ankit Srivastava <ankitks1512@gmail.com>
2016-07-01 20:21:10 +0530

    Update major_GUI_side_accept.sh

--- GUI_side_files/major_GUI_side_accept.sh
+++ GUI_side_files/major_GUI_side_accept.sh
@@ -1,4 +1,4 @@
-curl -i -H "Accept: application/json" -H "Content-Type: application/json" GET https://api.thingspeak.com/talkbacks/4578/commands/execute.json?api_key=R7333DX3WOMRQMF7 | json command_string > new_sensor_output.txt
+curl -i -H "Accept: application/json" -H "Content-Type: application/json" GET https://api.thingspeak.com/talkbacks/"your channel id"/commands/execute.json?api_key="your API key" | json command_string > new_sensor_output.txt
 
 sed '1,18d' new_sensor_output.txt >> /home/ankit/sketchbook/major_2_gui__/sensor_output.txt
 #sleep 3
